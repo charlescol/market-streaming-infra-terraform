2077ba61f285a48403a73b0f43fe0ddbe6db1824
charlescol <charlescol@hotmail.fr>
2025-06-23 10:45:12 +0200

    feat: policy for artifact and gce

--- infra/artifact_registry.tf
+++ infra/artifact_registry.tf
@@ -5,4 +5,22 @@ resource "google_artifact_registry_repository" "repos" {
   location      = var.region
   description   = "Docker registry for ${each.value}"
   format        = "DOCKER"
+
+  cleanup_policies {
+    id     = "delete-untagged"
+    action = "DELETE"
+    condition {
+      tag_state  = "UNTAGGED"
+      older_than = "1d"
+    }
+  }
+
+  cleanup_policies {
+    id     = "keep-minimum-versions"
+    action = "KEEP"
+    most_recent_versions {
+      package_name_prefixes = [""]
+      keep_count            = 3
+    }
+  }
 }
--- infra/storage.tf
+++ infra/storage.tf
@@ -6,3 +6,36 @@ resource "google_compute_disk" "gce-pv-disk" {
   zone                      = var.zone
   project                   = var.project_id
 }
+
+
+resource "google_compute_resource_policy" "gce_pv_disk_backup" {
+  name   = "gce-pv-disk-backup"
+  region = var.region
+
+  snapshot_schedule_policy {
+    schedule {
+      daily_schedule {
+        days_in_cycle = 1
+        start_time    = "15:00"
+      }
+    }
+    retention_policy {
+      max_retention_days    = 3
+      on_source_disk_delete = "APPLY_RETENTION_POLICY"
+    }
+    snapshot_properties {
+      guest_flush = true
+      labels = {
+        application = "grafana"
+        db_type     = "sqlite"
+      }
+    }
+  }
+}
+
+resource "google_compute_disk_resource_policy_attachment" "gce_pv_disk_attachment" {
+  name    = google_compute_resource_policy.gce_pv_disk_backup.name
+  disk    = google_compute_disk.gce-pv-disk.name
+  zone    = google_compute_disk.gce-pv-disk.zone
+  project = var.project_id
+}
