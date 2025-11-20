c886d8e0adb3b0b5843de21c893b63ee177590cc
charlescol <charlescol@hotmail.fr>
2025-11-20 10:17:42 +0100

    feat: update grafana IP resource to use global address

--- infra/services.tf
+++ infra/services.tf
@@ -1,11 +1,10 @@
 //  gcloud compute addresses describe grafana-ip --region=asia-northeast1 --project=market-streaming-prod --format="get(address)" 
-resource "google_compute_address" "grafana_ip" {
-  name         = "grafana-ip"
-  project      = var.project_id
-  region       = var.region
-  address_type = "EXTERNAL"
+
+resource "google_compute_global_address" "grafana_global_ip" {
+  name    = "grafana-global-ip"
+  project = var.project_id
 }
 
-output "grafana_ip" {
-  value = google_compute_address.grafana_ip.address
+output "grafana_global_ip" {
+  value = google_compute_global_address.grafana_global_ip.address
 }
