54ad050942168846d59927b42d9ad2bd0bf26d58
charlescol <charlescol@hotmail.fr>
2026-02-27 12:28:00 +0100

    feat: postgres_metadata_storage

--- infra/google_storage_bucket.tf
+++ infra/google_storage_bucket.tf
@@ -6,3 +6,12 @@ resource "google_storage_bucket" "druid_storage" {
   force_destroy = true
   versioning { enabled = true }
 }
+
+resource "google_storage_bucket" "postgres_metadata_storage" {
+  name                        = "postgres-metadata-storage-${var.project_id}"
+  location                    = var.region
+  uniform_bucket_level_access = true
+
+  force_destroy = true
+  versioning { enabled = true }
+}
